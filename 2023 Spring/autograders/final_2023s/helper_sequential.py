587115d72fe4b7d26624fc8b2b7c834ccf6d8dad
Mani Srivastava <mbs@ucla.edu>
2023-06-26 11:49:01 +0900

    Update helper_sequential.py

--- 2023 Spring/autograders/final_2023s/helper_sequential.py
+++ 2023 Spring/autograders/final_2023s/helper_sequential.py
@@ -9,11 +9,11 @@ import helper_task
 def validate_endstate_func(p,state,msg=[]):
     return helper_task.validate_endstate_func(p,state,msg)
 
-def validate_effort_func(p,state):
-    return helper_task.validate_effort_func(p,state)
+def validate_effort_func(p,state,has_fatal_error):
+    return helper_task.validate_effort_func(p,state,has_fatal_error)
 
-def runtimestats_func(p,state,has_fatal_error):
-    return helper_task.runtimestats_func(p,state,has_fatal_error)
+def runtimestats_func(p,state):
+    return helper_task.runtimestats_func(p,state)
 
 def gsheetstats_func(p,state,total_errors,has_fatal_error):
     return helper_task.gsheetstats_func(p,state,total_errors,has_fatal_error)
